db8be627e3b2b9a7942927a40d0a7e91ce57e7ee
Jamerson Felipe Pereira Paz <ljames666@gmail.com>
2025-05-17 16:04:05 -0300

    ci: nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,25 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                has: [{ type: 'host', value: 'app.linkmetur.com.br' }],
-                permanent: false,
-            },
-        ];
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                has: [{ type: 'host', value: 'app.linkmetur.com.br' }],
-            },
-        ];
-    },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/',
+    //             has: [{ type: 'host', value: 'app.linkmetur.com.br' }],
+    //             permanent: false,
+    //         },
+    //     ];
+    // },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/',
+    //             has: [{ type: 'host', value: 'app.linkmetur.com.br' }],
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
